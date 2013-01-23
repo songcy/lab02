d0ab3e715f45ad25e93c9339d113c7c7f9385a3e
Christine Song <songcy@usc.edu>
2013-01-23 14:57:55 -0800

    Update lab02.cpp

--- lab02.cpp
+++ lab02.cpp
@@ -28,7 +28,42 @@ Item::Item()
 }
 
 // Complete the Item class methods here
+Item::Item(string name, int qty, double price)
+{
+  name_ = name;
+	qty_ = qty;
+	price_ = price;
+}
 
+string Item::getName() const
+{
+	return name_;
+}
+
+int Item::getQty() const
+{
+	return qty_;
+}
+
+double Item::getPrice() const
+{
+	return price_;
+}
+
+void Item::setName(string name)
+{
+	name_ = name;
+}
+
+void Item::setQty(int qty)
+{
+	qty_ = qty;
+}
+
+void Item::setPrice(double price)
+{
+	price_ = price;
+}
 
 // Prototypes
 void printItems(const Item *items, const int len);
@@ -45,7 +80,7 @@ int main(int argc, char *argv[])
   Item *dataArray = readData(argv[1], len); 
   if(dataArray == NULL){
     cout << "Unable to open file: " << argv[1] << endl;
-    return 1;  
+    return 1;	
   }
   cout << "Read " << len << " Items from the file: " << argv[1] << endl;
   printItems(dataArray, len);
@@ -58,7 +93,16 @@ int main(int argc, char *argv[])
 // Print Item information in a tabular format
 void printItems(const Item *items, int len)
 {
-
+    cout << setw(15) << "Name";
+    cout << setw(10) << "Qty";
+    cout << setw(10) << "Price" << endl;
+    
+    for (int n = 0; n < len; n++)
+    {
+        cout << setw(15) << items[n].getName();
+        cout << setw(10) << items[n].getQty();
+        cout << setw(10) << setprecision(2) << items[n].getPrice() << endl;
+    }
 }
 
 
@@ -67,6 +111,33 @@ void printItems(const Item *items, int len)
 //  argument.
 Item *readData(const char *filename, int &len)
 {
- 
-  return NULL;
+  	string tname;
+	int tqty;
+	double tprice;
+    
+	ifstream myfile (filename);
+
+    
+    if (myfile.is_open())
+    {
+		myfile >> len;
+        Item *itemArray = new Item[len];
+        
+        
+            for (int i =0; i < len; i++)
+            {
+                myfile >> tname;
+				itemArray[i].setName(tname);
+                myfile >> tqty;
+				itemArray[i].setQty(tqty);
+                myfile >> tprice;
+				itemArray[i].setPrice(tprice);
+            }
+	
+        myfile.close();
+        return itemArray;
+    }
+    else
+        return NULL;
+    
 }
